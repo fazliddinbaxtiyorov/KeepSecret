638c416dcad47eff174ef36c977414aa8b0bc3ad
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2023-12-17 14:24:47 +0500

    Adding: models.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    like = models.IntegerField(default=0)
+    comment = models.IntegerField(default=0)
+    hashtag = models.CharField(max_length=255)
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    answer_text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
