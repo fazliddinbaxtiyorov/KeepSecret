e2c9e72e9cfa9e668ae767350c686194ac416397
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2023-12-17 13:46:24 +0500

    Adding: Users urls.py

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import register, Login, profile
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('signup/', register, name='sign'),
+    path('profile/', profile, name='profile'),
+    path('login/', Login.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout')
+
+]
