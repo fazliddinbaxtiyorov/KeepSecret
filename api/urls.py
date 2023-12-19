6b05433b63b2ebf3bcc247a56dbdf228bb531968
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2023-12-19 22:02:12 +0500

    Update: urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import PostListView, adding_question, search, QuestionDetail, add_comment
+from .views import PostListView, adding_question, search, QuestionDetail, add_comment, LikeView
 
 urlpatterns = [
     path('quests/', PostListView, name='home'),
     path('detail/<int:pk>/', QuestionDetail.as_view(), name='detail'),
     path('new_question/', adding_question, name='post_new'),
     path('new_answer/<int:pk>', add_comment, name='new'),
-    path('search/', search, name='search')
+    path('search/', search, name='search'),
+    path('like/<int:pk>/', LikeView, name='like'),
+
 ]
