09bb5e7601c9a0f0caf929538cc2ab7b763ea8f8
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2023-12-21 22:11:38 +0500

    Deleting: User in forms

--- api/forms.py
+++ api/forms.py
@@ -5,10 +5,10 @@ from .models import Question, Answer
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = ('user', 'question_text', 'hashtag')
+        fields = ('question_text', 'hashtag')
 
 
 class AnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
-        fields = ('user', 'answer_text')
+        fields = ('answer_text',)
